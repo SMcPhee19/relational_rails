748e8ca8774a30888fb602b768f365b23c3bce8c
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-03-24 17:34:27 -0600

    Add: Roster_count method

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,10 @@ class Team < ApplicationRecord
   has_many :players
 
   def self.most_recent
-    # require 'pry'; binding.pry
     order("created_at desc")
+  end
+
+  def roster_count
+    self.players.count
   end
 end
