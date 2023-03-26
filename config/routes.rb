ece75a74cd55b27429518af78ad92d62e5d8674e
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-03-26 14:47:08 -0600

    Add: Routes for User Story 11

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/teams', to: 'teams#index'
   get '/players', to: 'players#index'
+  get '/teams/new', to: 'teams#new'
   get '/teams/:id', to: 'teams#show'
   get '/players/:id', to: 'players#show'
   get '/teams/:team_id/players', to: 'team_players#index'
+  post '/teams', to: 'teams#create'
 end
