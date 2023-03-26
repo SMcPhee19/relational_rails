c8059e8c09b64ef8640ca72da75a445809e527c0
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-03-26 14:48:58 -0600

    Add: Create and New methods for US 11

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,4 +7,22 @@ class TeamsController < ApplicationController
     @teams = Team.find(params[:id])
     @players = @teams.players
   end
+
+  def new 
+  end
+
+  def create
+    # require 'pry'; binding.pry
+    team = Team.create!({
+      city: params["City"],
+      team_name: params["Team Name"],
+      stanley_cup_champ: params["Stanley Cup Champion"],
+      points: params["Points"],
+      division: params["Division"]
+      })
+      
+    team.save 
+
+    redirect_to '/teams'
+  end
 end
