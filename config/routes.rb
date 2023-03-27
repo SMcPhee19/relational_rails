86b5b129d720c7d7cfa1c5f3a475cca7df87a27a
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-03-26 20:28:33 -0600

    Add: Routes for User Story 13

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   get '/teams/new', to: 'teams#new'
   get '/teams/:id', to: 'teams#show'
   get '/players/:id', to: 'players#show'
-  get '/teams/:team_id/players', to: 'team_players#index'
+  get '/teams/:id/players', to: 'team_players#index'
   post '/teams', to: 'teams#create'
   get '/teams/:id/edit', to: 'teams#edit'
   patch '/teams/:id', to: 'teams#update'
+  get '/teams/:id/players/new', to: 'team_players#new'
+  post '/teams/:id/players/new', to: 'team_players#create'
 end
