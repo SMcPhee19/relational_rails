bb0d88fb098d67dd59208679a0b4f2e85a9c3925
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-03-28 16:54:41 -0600

    Refactor: #User Story [n] is located

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -32,13 +32,13 @@ RSpec.describe Team, type: :model do
       jersey_number: 4,
       position: "Defense")}
 
-    it '#most_recent' do
       # User Story 6
+    it '#most_recent' do
       expect(Team.most_recent).to eq([seattle_kraken, colorado_avalanche])
     end
 
+    #User Story 7
     it '#roster_count' do
-      #User Story 7
       expect(colorado_avalanche.roster_count).to eq(2)
     end
   end
