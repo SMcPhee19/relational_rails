9b5fa7290318e0303c65cfee26d892d1b261c799
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-03-28 16:57:29 -0600

    Test: Model test for User story 15

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -4,4 +4,37 @@ RSpec.describe Player, type: :model do
   describe "relationships" do
     it { should belong_to :team }
   end
+
+  describe 'class methods' do
+    let!(:colorado_avalanche) {Team.create!(city: "Denver",
+      team_name: "Avalanche",
+      stanley_cup_champ: true,
+      points: 119,
+      division: "Central" )}
+      
+    let!(:gabe) {colorado_avalanche.players.create!(name: "Gabriel Landeskog", 
+        birthplace: "Stockholm, SWE",
+        league_award_winner: true,
+        jersey_number: 92,
+        position: "Left Wing",)}
+        
+    let!(:bo) {colorado_avalanche.players.create!(name: "Bowen Byram",
+          birthplace: "Cranbrook, BC, CAN",
+          league_award_winner: false,
+          jersey_number: 4,
+          position: "Defense")}
+
+    let!(:cale) {colorado_avalanche.players.create!(name: "Cale Makar",
+          birthplace: "Calgary, AB, CAN",
+          league_award_winner: true,
+          jersey_number: 8,
+          position: "Defense")}
+
+    # User Story 9
+    it '#award_winner' do
+      # require 'pry'; binding.pry
+       expect(Player.award_winner).to eq([gabe, cale])
+    end
+
+  end
 end
